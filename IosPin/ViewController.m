ca273609019066b36fde6fa21451a4c6e7a6a120
Roshan Lamichhane <schoolwork.roshan@gmail.com>
2015-02-09 12:48:22 -0600

    First

--- IosPin/ViewController.m
+++ IosPin/ViewController.m
@@ -8,15 +8,15 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
-@end
 
 @implementation ViewController
-
+@synthesize first, second;
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +24,8 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+
+- (IBAction)submit:(id)sender {
+    }
 @end
---
+++ IosPin/ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  IosPin
+//
+//  Created by Roshan Lamichhane on 2/9/15.
+//  Copyright (c) 2015 Roshan Lamichhane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController
+
+@end
